f9861ec63868a4de100aa82d0a0463c22d70ea0a
shreya-jaiswal37 <156057205+shreya-jaiswal37@users.noreply.github.com>
2025-06-01 16:59:11 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,12 +10,14 @@ variable "instances" {
     machine_type           = string
     boot_image             = string
     boot_disk_size         = number
+    boot_disk_type         = optional(string, "pd-standard") # default value can be given
     network                = string
     subnetwork             = string
     metadata               = map(string)
     tags                   = list(string)
     labels                 = optional(map(string), {})
     deletion_protection    = optional(bool, false)
+    enable_public_ip       = optional(bool, true)
     static_ip              = optional(string)
     service_account_email  = optional(string)
     service_account_scopes = optional(list(string), ["cloud-platform"])
