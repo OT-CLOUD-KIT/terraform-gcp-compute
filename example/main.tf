d501bc94f8688ae9641dca69a5a42ea5cd2ac280
shreya-jaiswal37 <156057205+shreya-jaiswal37@users.noreply.github.com>
2025-06-02 13:49:05 +0530

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,7 @@
 module "compute_instance" {
-  source     = "./module"
-  project_id = var.project_id
-  instances  = var.instances
+  source             = "./module"
+  project_id         = var.project_id
+  instances          = var.instances
+  default_network    = var.default_network
+  default_subnetwork = var.default_subnetwork
 }
